62b2d9e93bd77a174d2e54ccadafc3bc33701477
Tavas  Neuwirth <tavasneuwirth@tavass-mbp.lan>
2020-02-25 18:55:22 -0700

    working on footer and logo

--- src/Components/FooterComponent/FooterComponent.js
+++ src/Components/FooterComponent/FooterComponent.js
@@ -1,27 +1,26 @@
 // Footer for like social media and what not
 import React from "react";
 
-import './FooterComponent.css';
+import "./FooterComponent.css";
+
+import Facebook from "../../assets/facebook.png";
+import Instagram from "../../assets/instagram.jpeg";
 
 function FooterComponent() {
   return (
     <div>
-      <div className='contact-info'>
-        <div className='email-info'>
-            <div>Contact US</div>
-            <div>Email:</div>
-            <div>Email Here </div>
+      <div className="contact-info">
+        <div className="email-info">
+          <div>Contact US</div>
+          <div>Email:</div>
+          <div>Email Here </div>
         </div>
-        <div>
-            Instagram logo
-        </div>
-        <div>
-            FaceBook logo maybe if we make one
+        <div className='social-media-div'>
+          <img src={Instagram} alt="" className="social-media-icons" />
+          <img src={Facebook} alt="" className="social-media-icons" />
         </div>
       </div>
-      <div className='copyright-info'>
-          Copy right here 
-      </div>
+      <div className="copyright-info">Copy right here</div>
     </div>
   );
 }
